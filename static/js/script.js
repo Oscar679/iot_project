3d88e23876b941b185e27d1a2402f71c7fff2dc1
Oscar679 <oscarekberg@hotmail.com>
2025-06-17 10:54:50 +0000

    Automatic bash script for push and commit

--- static/js/script.js
+++ static/js/script.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             lastUpdated.innerHTML = "";
             lastUpdated.innerHTML = data.timestamp;
+            console.log(data.timestamp);
         } catch (error) {
             console.error(error.message);
         }
