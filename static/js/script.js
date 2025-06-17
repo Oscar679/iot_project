78d12d24c5553ca1c41e710c5f52f68ae741e8aa
Oscar679 <oscarekberg@hotmail.com>
2025-06-17 11:36:30 +0000

    Automatic bash script for push and commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,39 @@
 /* For reference: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementsByClassName("row");
     const currentTempBox = document.getElementById("currentTemp");
     const currentHumidityBox = document.getElementById("currentHumidity");
     const lastUpdated = document.querySelector(".lastUpdated");
+    const ctx = document.getElementById("tempChart").getContext("2d");
+
+    const tempChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [],
+            // labels: data.map(row => row.data.timestamp.toLocaleTimeString()),
+            datasets: [
+                {
+                    label: 'Temperature (°C)',
+                    data: [],
+                    backgroundColor: '#ff6b6b',
+                    //data: data.map(row => row.temperature)
+                }
+            ]
+        }, options: {
+            animation: false,
+            scales: {
+                x: { title: { display: true, text: 'Time' } },
+                y: { beginAtZero: true, title: { display: true, text: '°C' } }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                }
+            }
+        }
+    });
 
     async function fetchData() {
         const url = "/api/current";
@@ -25,6 +55,17 @@ document.addEventListener("DOMContentLoaded", () => {
             lastUpdated.textContent = "";
             lastUpdated.textContent = new Date(data.timestamp).toLocaleDateString() + " " + new Date(data.timestamp).toLocaleTimeString();
             console.log(data.timestamp);
+
+            const time = new Date(data.timestamp).toLocaleDateString() + " " + new Date(data.timestamp).toLocaleTimeString();
+            tempChart.data.labels.push(time);
+            tempChart.data.datasets[0].data.push(data.temperature)
+
+            if (tempChart.data.labels.length > 10) {
+                tempChart.data.labels.shift();
+                tempChart.data.datasets[0].data.shift();
+            }
+
+            tempChart.update();
         } catch (error) {
             console.error(error.message);
         }
@@ -32,4 +73,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetchData();
     setInterval(fetchData, 10_000);
+
 });
